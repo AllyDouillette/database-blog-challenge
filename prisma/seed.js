9071fd715da882217f2127b15029bc560f6ca004
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-15 20:44:34 +0100

    add  posts to seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -20,17 +20,47 @@ async function seed() {
     console.log(`${createdUsers.count} users created`, createdUsers);
 
     // Add your code here
-    const createProfile = await prisma.profile.create({
-        data: {
+    const createProfiles = await prisma.profile.createMany({
+        data: [
+            {
                 firstName: 'alice',
                 lastName: 'wondaaa',
                 userId: 1,
                 profileUrl: '',
                 biography: ''
+            },
+            {
+                firstName: 'alice',
+                lastName: 'Two',
+                userId: 2,
+                profileUrl: '',
+                biography: ''
             }
+        ]
+    })
+    
+    console.log('created profiles', createProfiles)
+    
+    const posts = []
+    for (let i = 0; i < 10; i++) {
+        for (let j = 1; j < 3; j++) {
+            posts.push({
+                userId: j,
+                title: i+'Ljsahdjahsdj',
+                content: 'ajsjhajdasd',
+                pictureUrl: '',
+                published: !!Math.random() > 0.5,
+                upvotes: parseInt(Math.random()*100)
+            })
+        }
+    }
+
+    const createPosts = await prisma.post.createMany({
+        data: posts
     })
 
-    console.log('created profile', createProfile)
+    console.log('created posts', createPosts)
+
     // Don't edit any of the code below this line
     process.exit(0);
 }
